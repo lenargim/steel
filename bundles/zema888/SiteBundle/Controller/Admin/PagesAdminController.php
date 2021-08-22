9fc810d53048cf19f6558f27846cb67620c2766f
vit <vit888@yandex.ru>
2021-08-22 12:11:19 +0500

    c:c

--- bundles/zema888/SiteBundle/Controller/Admin/PagesAdminController.php
+++ bundles/zema888/SiteBundle/Controller/Admin/PagesAdminController.php
@@ -4,9 +4,20 @@ namespace SiteBundle\Controller\Admin;
 
 
 use ZemaTreeBundle\Controller\TreeAdminController;
+
 //use RedCode\TreeBundle\Controller\TreeAdminController;
 
 class PagesAdminController extends TreeAdminController
 {
-
+    public function listAction() {
+        $this->admin->setListModes([
+                                       'tree' => [
+                                           'class' => 'fa fa-tree fa-fw',
+                                       ],
+                                       'list' => [
+                                           'class' => 'fa fa-list fa-fw',
+                                       ],
+                                   ]);
+        return parent::listAction();
+    }
 }
